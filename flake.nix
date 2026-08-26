c947223940e291b99df58fbfc318afe68384967d
Markus Brandstätter <markus@brandstaetter.rocks>
2026-08-26 23:26:15 +0200

    chore(nix): update pnpm dependency hash for v3.0.0

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
               inherit (finalAttrs) pname version src;
               pnpm = pkgs.pnpm_9;
               fetcherVersion = 3;
-              hash = "sha256-+qGFLSVLJ9faZOmfO6ZVBP525i5LRgwhsJat2vT7Aw8=";
+              hash = "sha256-EHEwWEJ+78X2CySUbBrj+INX5StYSyzWzRtH+e6H9CQ=";
             };
 
             nativeBuildInputs = with pkgs; [
